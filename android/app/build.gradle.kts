a67a6f0361729f58448787ebfbb1185b8720cbef
babies <babies@Mac.attlocal.net>
2026-04-08 17:22:24 -0700

    Target API level 35 for Play Store requirement

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.picturnary.game"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.picturnary.game"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
